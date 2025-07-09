43e601cb7914f090d4df8647c97253807b9d31df
ADMIN <ADMIN@DESKTOP-TQECIC5>
2025-07-09 14:15:09 +0300

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,291 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button17 = New Button()
+        Button19 = New Button()
+        Button20 = New Button()
+        Button21 = New Button()
+        Button22 = New Button()
+        Button23 = New Button()
+        Button24 = New Button()
+        Button25 = New Button()
+        Button26 = New Button()
+        Button27 = New Button()
+        Button28 = New Button()
+        Button29 = New Button()
+        Button30 = New Button()
+        Button31 = New Button()
+        Button32 = New Button()
+        Button33 = New Button()
+        Button34 = New Button()
+        TextBox1 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Button17
+        ' 
+        Button17.BackColor = SystemColors.ActiveCaption
+        Button17.Font = New Font("Microsoft Sans Serif", 27.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button17.Location = New Point(12, 60)
+        Button17.Name = "Button17"
+        Button17.Size = New Size(75, 72)
+        Button17.TabIndex = 0
+        Button17.Text = "1"
+        Button17.UseVisualStyleBackColor = False
+        ' 
+        ' Button19
+        ' 
+        Button19.BackColor = SystemColors.ActiveCaption
+        Button19.Font = New Font("Microsoft Sans Serif", 27.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button19.Location = New Point(93, 60)
+        Button19.Name = "Button19"
+        Button19.Size = New Size(75, 72)
+        Button19.TabIndex = 0
+        Button19.Text = "2"
+        Button19.UseVisualStyleBackColor = False
+        ' 
+        ' Button20
+        ' 
+        Button20.BackColor = SystemColors.ActiveCaption
+        Button20.Font = New Font("Microsoft Sans Serif", 27.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button20.Location = New Point(12, 138)
+        Button20.Name = "Button20"
+        Button20.Size = New Size(75, 72)
+        Button20.TabIndex = 0
+        Button20.Text = "4"
+        Button20.UseVisualStyleBackColor = False
+        ' 
+        ' Button21
+        ' 
+        Button21.BackColor = SystemColors.ActiveCaption
+        Button21.Font = New Font("Microsoft Sans Serif", 27.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button21.Location = New Point(174, 60)
+        Button21.Name = "Button21"
+        Button21.Size = New Size(75, 72)
+        Button21.TabIndex = 0
+        Button21.Text = "3"
+        Button21.UseVisualStyleBackColor = False
+        ' 
+        ' Button22
+        ' 
+        Button22.BackColor = SystemColors.ActiveCaption
+        Button22.Font = New Font("Microsoft Sans Serif", 27.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button22.Location = New Point(12, 216)
+        Button22.Name = "Button22"
+        Button22.Size = New Size(75, 72)
+        Button22.TabIndex = 0
+        Button22.Text = "7"
+        Button22.UseVisualStyleBackColor = False
+        ' 
+        ' Button23
+        ' 
+        Button23.BackColor = SystemColors.ActiveCaption
+        Button23.Font = New Font("Microsoft Sans Serif", 27.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button23.Location = New Point(93, 138)
+        Button23.Name = "Button23"
+        Button23.Size = New Size(75, 72)
+        Button23.TabIndex = 0
+        Button23.Text = "5"
+        Button23.UseVisualStyleBackColor = False
+        ' 
+        ' Button24
+        ' 
+        Button24.BackColor = SystemColors.ActiveCaption
+        Button24.Font = New Font("Microsoft Sans Serif", 27.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button24.Location = New Point(12, 294)
+        Button24.Name = "Button24"
+        Button24.Size = New Size(75, 72)
+        Button24.TabIndex = 0
+        Button24.Text = "0"
+        Button24.UseVisualStyleBackColor = False
+        ' 
+        ' Button25
+        ' 
+        Button25.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(128))
+        Button25.Font = New Font("Microsoft Sans Serif", 27.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button25.Location = New Point(255, 60)
+        Button25.Name = "Button25"
+        Button25.Size = New Size(75, 72)
+        Button25.TabIndex = 0
+        Button25.Text = "+"
+        Button25.UseVisualStyleBackColor = False
+        ' 
+        ' Button26
+        ' 
+        Button26.BackColor = SystemColors.ActiveCaption
+        Button26.Font = New Font("Microsoft Sans Serif", 27.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button26.Location = New Point(93, 216)
+        Button26.Name = "Button26"
+        Button26.Size = New Size(75, 72)
+        Button26.TabIndex = 0
+        Button26.Text = "8"
+        Button26.UseVisualStyleBackColor = False
+        ' 
+        ' Button27
+        ' 
+        Button27.BackColor = SystemColors.ActiveCaption
+        Button27.Font = New Font("Microsoft Sans Serif", 27.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button27.Location = New Point(174, 138)
+        Button27.Name = "Button27"
+        Button27.Size = New Size(75, 72)
+        Button27.TabIndex = 0
+        Button27.Text = "6"
+        Button27.UseVisualStyleBackColor = False
+        ' 
+        ' Button28
+        ' 
+        Button28.BackColor = SystemColors.ActiveCaption
+        Button28.Font = New Font("Microsoft Sans Serif", 27.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button28.Location = New Point(93, 294)
+        Button28.Name = "Button28"
+        Button28.Size = New Size(75, 72)
+        Button28.TabIndex = 0
+        Button28.Text = "."
+        Button28.UseVisualStyleBackColor = False
+        ' 
+        ' Button29
+        ' 
+        Button29.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(128))
+        Button29.Font = New Font("Microsoft Sans Serif", 27.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button29.Location = New Point(255, 138)
+        Button29.Name = "Button29"
+        Button29.Size = New Size(75, 72)
+        Button29.TabIndex = 0
+        Button29.Text = "-"
+        Button29.UseVisualStyleBackColor = False
+        ' 
+        ' Button30
+        ' 
+        Button30.BackColor = SystemColors.ActiveCaption
+        Button30.Font = New Font("Microsoft Sans Serif", 27.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button30.Location = New Point(174, 216)
+        Button30.Name = "Button30"
+        Button30.Size = New Size(75, 72)
+        Button30.TabIndex = 0
+        Button30.Text = "9"
+        Button30.UseVisualStyleBackColor = False
+        ' 
+        ' Button31
+        ' 
+        Button31.BackColor = SystemColors.ActiveCaption
+        Button31.Font = New Font("Microsoft Sans Serif", 27.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button31.Location = New Point(174, 294)
+        Button31.Name = "Button31"
+        Button31.Size = New Size(75, 72)
+        Button31.TabIndex = 0
+        Button31.Text = "c"
+        Button31.UseVisualStyleBackColor = False
+        ' 
+        ' Button32
+        ' 
+        Button32.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(128))
+        Button32.Font = New Font("Microsoft Sans Serif", 27.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button32.Location = New Point(255, 216)
+        Button32.Name = "Button32"
+        Button32.Size = New Size(75, 72)
+        Button32.TabIndex = 0
+        Button32.Text = "*"
+        Button32.UseVisualStyleBackColor = False
+        ' 
+        ' Button33
+        ' 
+        Button33.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(128))
+        Button33.Font = New Font("Microsoft Sans Serif", 27.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button33.Location = New Point(255, 294)
+        Button33.Name = "Button33"
+        Button33.Size = New Size(75, 72)
+        Button33.TabIndex = 0
+        Button33.Text = "/"
+        Button33.UseVisualStyleBackColor = False
+        ' 
+        ' Button34
+        ' 
+        Button34.BackColor = Color.PaleGreen
+        Button34.Font = New Font("Microsoft Sans Serif", 27.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button34.Location = New Point(12, 372)
+        Button34.Name = "Button34"
+        Button34.Size = New Size(318, 72)
+        Button34.TabIndex = 0
+        Button34.Text = "="
+        Button34.UseVisualStyleBackColor = False
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BackColor = Color.PaleGreen
+        TextBox1.Font = New Font("Microsoft Sans Serif", 27.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        TextBox1.Location = New Point(12, 5)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(318, 49)
+        TextBox1.TabIndex = 1
+        TextBox1.Text = "0"
+        TextBox1.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(343, 450)
+        Controls.Add(TextBox1)
+        Controls.Add(Button34)
+        Controls.Add(Button33)
+        Controls.Add(Button32)
+        Controls.Add(Button31)
+        Controls.Add(Button30)
+        Controls.Add(Button29)
+        Controls.Add(Button28)
+        Controls.Add(Button27)
+        Controls.Add(Button26)
+        Controls.Add(Button25)
+        Controls.Add(Button24)
+        Controls.Add(Button23)
+        Controls.Add(Button22)
+        Controls.Add(Button21)
+        Controls.Add(Button20)
+        Controls.Add(Button19)
+        Controls.Add(Button17)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Button19 As Button
+    Friend WithEvents Button20 As Button
+    Friend WithEvents Button21 As Button
+    Friend WithEvents Button22 As Button
+    Friend WithEvents Button23 As Button
+    Friend WithEvents Button24 As Button
+    Friend WithEvents Button25 As Button
+    Friend WithEvents Button26 As Button
+    Friend WithEvents Button27 As Button
+    Friend WithEvents Button28 As Button
+    Friend WithEvents Button29 As Button
+    Friend WithEvents Button30 As Button
+    Friend WithEvents Button31 As Button
+    Friend WithEvents Button32 As Button
+    Friend WithEvents Button33 As Button
+    Friend WithEvents Button34 As Button
+    Friend WithEvents TextBox1 As TextBox
+
+End Class
---
+++ calculator app java.vb
@@ -0,0 +1,150 @@
+﻿Public Class Form1
+
+    Dim Firstnum As Decimal
+    Dim Secondnum As Decimal
+    Dim Operations As Integer
+    Dim Operator_Selector As Boolean = False
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "1"
+        Else
+            TextBox1.Text = "1"
+        End If
+    End Sub
+
+    Private Sub Button19_Click(sender As Object, e As EventArgs) Handles Button19.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "2"
+        Else
+            TextBox1.Text = "2"
+        End If
+    End Sub
+
+    Private Sub Button21_Click(sender As Object, e As EventArgs) Handles Button21.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "3"
+        Else
+            TextBox1.Text = "3"
+        End If
+    End Sub
+
+    Private Sub Button20_Click(sender As Object, e As EventArgs) Handles Button20.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "4"
+        Else
+            TextBox1.Text = "4"
+        End If
+    End Sub
+
+    Private Sub Button23_Click(sender As Object, e As EventArgs) Handles Button23.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "5"
+        Else
+            TextBox1.Text = "5"
+        End If
+    End Sub
+
+    Private Sub Button27_Click(sender As Object, e As EventArgs) Handles Button27.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "6"
+        Else
+            TextBox1.Text = "6"
+        End If
+    End Sub
+
+    Private Sub Button22_Click(sender As Object, e As EventArgs) Handles Button22.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "7"
+        Else
+            TextBox1.Text = "7"
+        End If
+    End Sub
+
+    Private Sub Button26_Click(sender As Object, e As EventArgs) Handles Button26.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "8"
+        Else
+            TextBox1.Text = "8"
+        End If
+    End Sub
+
+    Private Sub Button30_Click(sender As Object, e As EventArgs) Handles Button30.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "9"
+        Else
+            TextBox1.Text = "9"
+        End If
+    End Sub
+
+    Private Sub Button24_Click(sender As Object, e As EventArgs) Handles Button24.Click
+        If TextBox1.Text <> "0" Then
+            TextBox1.Text += "0"
+        End If
+    End Sub
+
+    Private Sub Button28_Click(sender As Object, e As EventArgs) Handles Button28.Click
+        If Not TextBox1.Text.Contains(".") Then
+            TextBox1.Text += "."
+        End If
+    End Sub
+
+    Private Sub Button31_Click(sender As Object, e As EventArgs) Handles Button31.Click
+        TextBox1.Text = "0"
+    End Sub
+
+    Private Sub Button25_Click(sender As Object, e As EventArgs) Handles Button25.Click
+        Firstnum = TextBox1.Text
+        TextBox1.Text = "0"
+        Operator_Selector = True
+        Operations = 1 ' = +
+    End Sub
+
+    Private Sub Button29_Click(sender As Object, e As EventArgs) Handles Button29.Click
+        Firstnum = TextBox1.Text
+        TextBox1.Text = "0"
+        Operator_Selector = True
+        Operations = 2 ' = -
+    End Sub
+
+    Private Sub Button32_Click(sender As Object, e As EventArgs) Handles Button32.Click
+        Firstnum = TextBox1.Text
+        TextBox1.Text = "0"
+        Operator_Selector = True
+        Operations = 3 ' = *
+    End Sub
+
+    Private Sub Button33_Click(sender As Object, e As EventArgs) Handles Button33.Click
+        Firstnum = TextBox1.Text
+        TextBox1.Text = "0"
+        Operator_Selector = True
+        Operations = 4 ' = /
+    End Sub
+
+    Private Sub Button34_Click(sender As Object, e As EventArgs) Handles Button34.Click
+        If Operator_Selector = True Then
+            Secondnum = TextBox1.Text
+            Select Case Operations
+                Case 1 ' +
+                    TextBox1.Text = Firstnum + Secondnum
+                Case 2 ' -
+                    TextBox1.Text = Firstnum - Secondnum
+                Case 3 ' *
+                    TextBox1.Text = Firstnum * Secondnum
+                Case 4 ' /
+                    If Secondnum <> 0 Then
+                        TextBox1.Text = Firstnum / Secondnum
+                    Else
+                        MessageBox.Show("Cannot divide by zero!")
+                        TextBox1.Text = "0"
+                    End If
+            End Select
+            Operator_Selector = False
+
+        End If
+    End Sub
+End Class
